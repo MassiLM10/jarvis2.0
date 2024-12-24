4393940d604724ca2bfa03983bf18716cd16ad74
MassiLM10 <massi.leo.10r@gmail.com>
2024-12-24 15:10:54 +0530

    day-3

--- www/main.js
+++ www/main.js
@@ -10,4 +10,34 @@ $(document).ready(function () {
         },
 
     });
+    //siri configurstion
+    var siriWave = new SiriWave({
+        container: document.getElementById("siri-container"),
+        width: 800,
+        height: 200,
+        style:"ios9",
+        amplitude:"1",
+        speed: "0.30",
+        autostart: true
+        
+
+      });
+
+      // siri message animation
+      $('.siri-meessage').textillate({
+        loop:true,
+        sync:true,
+        in:{
+            effect: "fadeInUp",
+            sync:true,
+        },
+        out:{
+            effect: "fadeOutUp",
+            sync:true,
+        },
+
+    });
+
+
+
 });
